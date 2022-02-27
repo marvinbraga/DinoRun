5a752df11e6c6f7ac23ff886d383a5d299e4472a
Marcus Braga <mvbraga@gmail.com>
2022-02-27 00:54:27 -0300

    Ground

--- core/background_manager.py
+++ core/background_manager.py
@@ -6,10 +6,46 @@ from core.artefacts import Artefact
 
 
 class BackgroundMoveType(Enum):
-    TOP_DOWN = 0
-    DOWN_TOP = 1
+    TOP_BOTTOM = 0
+    BOTTOM_TOP = 1
     LEFT_RIGHT = 2
     RIGHT_LEFT = 3
+
+    def get_movement(self):
+        return {
+            BackgroundMoveType.RIGHT_LEFT: MoveRight2Left,
+            BackgroundMoveType.TOP_BOTTOM: MoveRight2Left,
+            BackgroundMoveType.LEFT_RIGHT: MoveRight2Left,
+            BackgroundMoveType.BOTTOM_TOP: MoveRight2Left,
+        }[self]
+
+    def get_positions(self, background):
+        return {
+            BackgroundMoveType.TOP_BOTTOM: (background.x, background.y - background.height),
+            BackgroundMoveType.BOTTOM_TOP: (background.x, background.y + background.height),
+            BackgroundMoveType.LEFT_RIGHT: (background.x - background.width, background.y),
+            BackgroundMoveType.RIGHT_LEFT: (background.x + background.width, background.y),
+        }[self]
+
+
+class MoveRight2Left:
+
+    def __init__(self, image1, image2, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image1 = image1
+        self.image2 = image2
+
+    def get_positions(self):
+        return self.x + self.width, self.y
+
+    def move(self, speed):
+        self.image1.rect[0] = self.image1.x \
+            if self.image1.rect[0] <= -self.width else self.image1.rect[0] - speed
+        self.image2.rect[0] = self.image2.x \
+            if self.image2.rect[0] <= self.image1.x else self.image2.rect[0] - speed
 
 
 class Background:
@@ -21,25 +57,23 @@ class Background:
         self.move_type = move_type
         self.height = height
         self.width = width
-        image2_position = self.get_image_position() 
+        image2_position = self.move_type.get_positions(self)
         self.image1 = Artefact(image, x, y, *groups)
         self.image2 = Artefact(image, *image2_position, *groups)
+        self.movement = self.move_type.get_movement()(self.image1, self.image2, x, y, width, height)
 
     def get_image_position(self):
         result = {
-            BackgroundMoveType.TOP_DOWN: (self.x, self.y - self.height),
-            BackgroundMoveType.DOWN_TOP: (self.x, self.y + self.height),
+            BackgroundMoveType.TOP_BOTTOM: (self.x, self.y - self.height),
+            BackgroundMoveType.BOTTOM_TOP: (self.x, self.y + self.height),
             BackgroundMoveType.LEFT_RIGHT: (self.x - self.width, self.y),
             BackgroundMoveType.RIGHT_LEFT: (self.x + self.width, self.y),
         }[self.move_type]
         return result
 
     def move(self):
-        self.image1.rect[0] = self.image1.x \
-            if self.image1.rect[0] <= -self.width else self.image1.rect[0] - self.speed
-        self.image2.rect[0] = self.image2.x \
-            if self.image2.rect[0] <= self.image1.x else self.image2.rect[0] - self.speed
-        
+        self.movement.move(self.speed)
+
     def update(self):
         self.move()
 
