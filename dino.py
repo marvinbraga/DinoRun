71734b42e6828eb13878c4385ee8997df1c074dc
Marcus Braga <mvbraga@gmail.com>
2022-02-28 21:38:06 -0300

    Dino ducking

--- dino.py
+++ dino.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 from enum import Enum
 
 import pygame
@@ -28,8 +28,13 @@ class AbstractStatus(metaclass=ABCMeta):
         return AnimatedArtefact.adjust_images(images, self.width, self.height)
 
 
-class DinoJump(AbstractStatus):
+class DinoRun(AbstractStatus):
 
+    def __init__(self, dino):
+        super().__init__(dino, ["sprites/dino_1.png", "sprites/dino_2.png"])
+
+
+class DinoJump(AbstractStatus):
     _UP = 9
     _GRAVITY = -0.25
     _TIME = 0
@@ -72,16 +77,19 @@ class DinoJump(AbstractStatus):
         return self
 
 
-class DinoRun(AbstractStatus):
-
-    def __init__(self, dino):
-        super().__init__(dino, ["sprites/dino_1.png", "sprites/dino_2.png"])
-
-
 class DinoDuck(AbstractStatus):
 
     def __init__(self, dino):
         super().__init__(dino, ["sprites/dino_ducking1.png", "sprites/dino_ducking2.png"])
+
+    def go(self):
+        if DinoStatus.DUCKING not in self.dino.status:
+            self.dino.status.append(DinoStatus.DUCKING)
+        return self
+
+    def set_complete(self):
+        self.dino.status.remove(DinoStatus.DUCKING)
+        return self
 
 
 class Dino(AnimatedArtefact):
@@ -92,7 +100,7 @@ class Dino(AnimatedArtefact):
         self.width, self.height = 44, 48
         self.tick_limit = 8
         self.status = [DinoStatus.RUNNING]
-        self.valid_keys = [pygame.K_SPACE]
+        self.valid_keys = [pygame.K_SPACE, pygame.K_DOWN]
 
         self.jump = DinoJump(self)
         self.run = DinoRun(self)
@@ -106,8 +114,14 @@ class Dino(AnimatedArtefact):
         return self
 
     def check_keys(self, event):
-        if event.key == pygame.K_SPACE:
-            self.jump.go()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                self.jump.go()
+            elif event.key == pygame.K_DOWN:
+                self.duck.go()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                self.duck.set_complete()
 
     def update(self):
         if DinoStatus.JUMPING in self.status:
@@ -118,3 +132,6 @@ class Dino(AnimatedArtefact):
         else:
             self.images = self.run.images
         super().update()
+
+    def is_collide(self, group, kill=False):
+        return pygame.sprite.spritecollide(self, group, kill)
